035394485eefc5ddda4914af8ade8bef30f502bd
毛 <1296193245@qq.com>
2022-03-08 12:38:00 +0800

    JS_Element

---
+++ target/JS_Element-1.0-SNAPSHOT/test1.js
@@ -0,0 +1,98 @@
+/**
+ * Project name(项目名称)：JS_Element
+ * File name(文件名): test1
+ * Author(作者）: mao
+ * Author QQ：1296193245
+ * GitHub：https://github.com/maomao124/
+ * Date(创建日期)： 2022/3/8
+ * Time(创建时间)： 12:17
+ * Version(版本): 1.0
+ * Description(描述)：
+ * Element 对象中的属性
+ 属性                描述
+ element.accessKey    设置或返回一个访问单选按钮的快捷键
+ element.attributes    返回一个元素的属性数组
+ element.childNodes    返回元素的一个子节点的数组
+ element.children    返回元素中子元素的集合
+ element.classList    返回元素中类名组成的对象
+ element.className    设置或返回元素的 class 属性
+ element.clientHeight    返回内容的可视高度（不包括边框，边距或滚动条）
+ element.clientWidth    返回内容的可视宽度（不包括边框，边距或滚动条）
+ element.contentEditable    设置或返回元素的内容是否可编辑
+ element.dir    设置或返回一个元素中的文本方向
+ element.firstChild    返回元素中的第一个子元素
+ element.id    设置或者返回元素的 id
+ element.innerHTML    设置或者返回元素的内容
+ element.isContentEditable    返回元素内容是否可编辑，如果可编辑则返回 true，否则返回 false
+ element.lang    设置或者返回一个元素的语言
+ element.lastChild    返回元素的最后一个子元素
+ element.namespaceURI    返回命名空间的 URI
+ element.nextSibling    返回指定元素之后的兄弟元素，两个元素在 DOM 树中位于同一层级（包括文本节点、注释节点）
+ element.nextElementSibling    返回指定元素之后的兄弟元素，两个元素在 DOM 树中位于同一层级（不包括文本节点、注释节点）
+ element.nodeName    返回元素名称（大写）
+ element.nodeType    返回元素的节点类型
+ element.nodeValue    返回元素的节点值
+ element.offsetHeight    返回元素的高度，包括边框和内边距，但不包括外边距
+ element.offsetWidth    返回元素的宽度，包括边框和内边距，但不包括外边距
+ element.offsetLeft    返回元素在水平方向的偏移量
+ element.offsetParent    返回距离该元素最近的进行过定位的父元素
+ element.offsetTop    返回元素在垂直方向的偏移量
+ element.ownerDocument    返回元素的根元素（文档对象）
+ element.parentNode    返回元素的父节点
+ element.previousSibling    返回元素之前的兄弟元素，两个元素在 DOM 树中位于同一层级（包括文本节点、注释节点）
+ element.previousElementSibling    返回元素之前的兄弟元素，两个元素在 DOM 树中位于同一层级（不包括文本节点、注释节点）
+ element.scrollHeight    返回元素的完整高度（包括被滚动条隐蔽的部分）
+ element.scrollLeft    设置或返回元素滚动条距离元素左侧的距离
+ element.scrollTop    设置或返回元素滚动条距离元素上方的距离
+ element.scrollWidth    返回元素的完整宽度（包括被滚动条隐蔽的部分）
+ element.style    设置或返回元素的样式属性
+ element.tabIndex    设置或返回元素的标签顺序
+ element.tagName    以字符的形式返回元素的名称（大写）
+ element.textContent    设置或返回某个元素以及其中的文本内容
+ element.title    设置或返回元素的 title 属性
+ element.length    返回对象的长度
+ */
+
+var element = document.getElementById("test1");
+
+//document.write(element.accessKey);
+//document.write(element.attributes);
+//document.write(element.childNodes);
+//document.write(element.children);
+//document.write(element.classList);
+// document.write(element.className);
+// document.write(element.clientHeight);
+// document.write(element.clientWidth);
+// document.write(element.contentEditable);
+// document.write(element.dir);
+// document.write(element.firstChild);
+// document.write(element.id);
+// document.write(element.innerHTML);
+// document.write(element.isContentEditable);
+// document.write(element.lang);
+// document.write(element.lastChild);
+// document.write(element.namespaceURI);
+// document.write(element.nextSibling);
+// document.write(element.nextElementSibling);
+// document.write(element.nodeName);
+// document.write(element.nodeType);
+// document.write(element.nodeValue);
+// document.write(element.offsetHeight);
+// document.write(element.offsetWidth);
+// document.write(element.offsetLeft);
+// document.write(element.offsetParent);
+// document.write(element.offsetTop);
+// document.write(element.ownerDocument);
+// document.write(element.parentNode);
+// document.write(element.previousSibling);
+// document.write(element.previousElementSibling);
+// document.write(element.scrollHeight);
+// document.write(element.scrollLeft);
+// document.write(element.scrollTop);
+// document.write(element.scrollWidth);
+// document.write(element.style);
+// document.write(element.tabIndex);
+// document.write(element.tagName);
+// document.write(element.textContent);
+// document.write(element.title);
+// document.write(element.length);
